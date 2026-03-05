5d54e7def5aecf09b2b20402e3c42d5d1f6cbe98
Nilesh Mohite <34605580+niluniverse@users.noreply.github.com>
2026-03-05 09:49:03 +0530

    fix(swagger): resolve package mismatch by adding Swashbuckle.AspNetCore

--- POCWebApplication/Program.cs
+++ POCWebApplication/Program.cs
@@ -1,17 +1,31 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+// Configure Swagger/OpenAPI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "POC Web Application API",
+        Version = "v1",
+        Description = "A sample ASP.NET Core Web API"
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "POC Web Application API v1");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
